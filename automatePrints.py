52bd8673954b833f51486b3dfbd9bc1f3feda8c6
xelinhabl <xelinhabl@gmail.com>
2024-02-23 11:00:07 -0300

    update_local_save_prints

--- automatePrints.py
+++ automatePrints.py
@@ -3,11 +3,14 @@ import time
 import os
 
 
-# Seta pasta para salvar os print's
-local_prints = os.listdir("C:\\Users\\alex.lopes\\projectsPython\\prints\\")
+# Seta pasta para salvar os print's pasta destino
+local_prints = "C:\\Users\\alex.lopes\\projectsPython\\prints\\"
+
+# Verifica a quantidade de print's dentro da pasta destino
+local_prints_qnt = os.listdir("C:\\Users\\alex.lopes\\projectsPython\\prints\\")
 
 # Verifica a quantidades de arquivos dentro da pasta e seta o nome do arquivo
-qnt_local_arq = len(local_prints)
+qnt_local_arq = len(local_prints_qnt)
 if qnt_local_arq == 0 :
     namePrint = 'screenshot%s' % qnt_local_arq
 else: 
@@ -18,9 +21,14 @@ else:
 img1 = pyautogui.screenshot()
 img1.save(r"C:\Users\alex.lopes\projectsPython\prints\%s.png" % namePrint)
 
+img1 = pyautogui.locateCenterOnScreen(r"C:\Users\alex.lopes\projectsPython\prints\%s.png" % namePrint)
+print (img1)
+
+pyautogui.hotkey('ctrl', 'c')
+
 # Verifica se o print foi salvo dentro da pasta
 salve_done = False
-if os.path.exists(r"C:\Users\alex.lopes\projectsPython\prints\%s.png" % namePrint):
+if os.path.exists(local_prints + "%s.png" % namePrint):
     salve_done = True 
     print('Arquivo salvo com sucesso ! ')
 
